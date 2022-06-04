a08c5c6d61269ea296954ba1cbd05aa47572cc5b
talhasaeed22 <talhasaeed100@gmail.com>
2022-06-05 01:55:53 +0500

    Final push

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
     }
     return (
         <div id='Navbar'>
-            <motion.div initial={{y:-250}} animate={{y:-10}} transition={{delay:0.5, duration:1, type:'spring', stiffness:130}} className='n-parent'  style={{ borderBottom: '1px solid #FCA61F', width:'100%', zIndex:'1000000', }}>
+            <motion.div initial={{y:-250}} animate={{y:-10}} transition={{delay:0.5, duration:1, type:'spring', stiffness:130}} className='n-parent'  style={{ borderBottom: '1px solid #FCA61F', width:'100%', zIndex:'1000000', backgroundColor:mode === 'dark' ? 'black': 'white'}}>
                 <div className="n-left">
                     <div className="name" style={{ fontSize: '1.6rem', fontWeight: 'bolder' }}>
                         M.Talha
